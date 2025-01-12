b69ebaca7b3f0297ca180f7097c7db162cd2f2b7
Bojan Crnovcic <bojan969@gmail.com>
2025-01-12 23:54:25 +0100

    charnge port 2

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   
   app.enableCors();
 
-  const port = DatabaseConfig.port || 3000;
+  const port = process.env.PORT|| 3000;
     
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
